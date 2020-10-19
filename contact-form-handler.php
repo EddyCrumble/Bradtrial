7827cf8a94d3933b36951e23fa9c337c894bca11
EddyCrumble <69426772+EddyCrumble@users.noreply.github.com>
2020-10-19 14:48:19 +0100

    Add files via upload

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,16 +1,19 @@
 <?php
-if(!empty($_POST["send"])) {
-	$name = $_POST["userName"];
-	$email = $_POST["userEmail"];
+if(!isset($_POST["send"])) {
+	$name = $_POST["name"];
+	$email = $_POST["email"];
 	$subject = $_POST["subject"];
-	$content = $_POST["content"];
+	$message = $_POST["message"];
 
-	$toEmail = "admin@phppot_samples.com";
+	$toEmail = "bradjhomes@gmail.com";
 	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
-	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	if(mail($toEmail, $subject, $message, $mailHeaders)) {
 	    $message = "Your contact information is received successfully.";
 	    $type = "success";
 	}
+	else{
+		$message = "Something went wrong!";
+	}
 }
-require_once "contact-view.php";
+
 ?>
